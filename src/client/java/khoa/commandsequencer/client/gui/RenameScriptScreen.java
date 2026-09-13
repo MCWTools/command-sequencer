f88122b51f0b1becb3eb09acda573136b940d44f
MCWTools <mcwchecker@gmail.com>
2026-09-13 13:20:06 +0700

    Fix text alpha, auto-reset timing, layout overflow, add /cs command + Run/Reset buttons

--- src/client/java/khoa/commandsequencer/client/gui/RenameScriptScreen.java
+++ src/client/java/khoa/commandsequencer/client/gui/RenameScriptScreen.java
@@ -48,7 +48,7 @@ public class RenameScriptScreen extends Screen {
 	public void extractRenderState(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float partialTick) {
 		super.extractRenderState(graphics, mouseX, mouseY, partialTick);
 		graphics.text(this.font, this.getTitle(), (this.width - this.font.width(this.getTitle())) / 2,
-				this.height / 2 - 30, 0xFFFFFF, true);
+				this.height / 2 - 30, 0xFFFFFFFF, true);
 	}
 
 	private void confirm() {
